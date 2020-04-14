b5844d5df6519c1de6b107f85ba468e6a537fcf9
nbc <nicolas@barna.be>
2020-04-14 17:21:03 +0200

    bugfix: fix owner of .htaccess

--- spip/start.sh
+++ spip/start.sh
@@ -1,1 +1,6 @@
-chown -R www-data:www-data /var/www/html/* && chmod -R g+w /var/www/html && apache2-foreground
+#!/bin/sh
+
+chown -R www-data:www-data /var/www/html/* && \
+    chown www-data:www-data /var/www/html/.htaccess && \
+    chmod -R g+w /var/www/html/* && \
+    apache2-foreground
