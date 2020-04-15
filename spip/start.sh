e8972b44c828a1c03d33b9e41f2f80df4ebb19bc
nbc <nicolas@barna.be>
2020-04-15 09:50:32 +0000

    bugfix: change htaccess.txt mod

--- spip/start.sh
+++ spip/start.sh
@@ -3,4 +3,5 @@
 chown -R www-data:www-data /var/www/html/* && \
     chown www-data:www-data /var/www/html/.htaccess && \
     chmod -R g+w /var/www/html/* && \
+    chmod g+w /var/www/html/htaccess.txt && \
     apache2-foreground
